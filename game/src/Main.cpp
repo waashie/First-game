4faaaed212fb08e3f333a5daf4153b5d10db9ce9
Cash MacKinnon <cashcam07@gmail.com>
2026-09-22 21:20:49 +0000

    Changed background color to red

--- game/src/Main.cpp
+++ game/src/Main.cpp
@@ -5,26 +5,32 @@
 
 #include "bn_sprite_items_dot.h"
 
-int main() {
+int main()
+{
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(0, 0, 0));
+    bn::backdrop::set_color(bn::color(31, 0, 0));
 
     auto dot = bn::sprite_items::dot.create_sprite(0, 0);
 
-    bn::fixed speed = 1.5;
+    bn::fixed speed = 5.5;
 
-    while(true) {
-        if(bn::keypad::left_held()) {
+    while (true)
+    {
+        if (bn::keypad::left_held())
+        {
             dot.set_x(dot.x() - speed);
         }
-        if(bn::keypad::right_held()) {
+        if (bn::keypad::right_held())
+        {
             dot.set_x(dot.x() + speed);
         }
-        if(bn::keypad::up_held()) {
+        if (bn::keypad::up_held())
+        {
             dot.set_y(dot.y() - speed);
         }
-        if(bn::keypad::down_held()) {
+        if (bn::keypad::down_held())
+        {
             dot.set_y(dot.y() + speed);
         }
         bn::core::update();
